e3f85549610dc7848f953e52f62ffb9431bec4d3
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-04-28 03:12:13 +0700

    change: topic attribute on LampData response

--- src/api/mqqt/types.ts
+++ src/api/mqqt/types.ts
@@ -1,5 +1,10 @@
+export type Lamp = {
+  topic: LampTopic;
+  state: "ON" | "OFF";
+};
+
+export type LampTopic = "gpio2" | "gpio16";
+
 export interface SmartLampData {
-  data: {
-    result: "ON" | "OFF";
-  };
+  data: Lamp[];
 }
