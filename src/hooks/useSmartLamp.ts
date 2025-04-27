323770871186b6f57b78eaa2ee6dcdad6c8f0ad7
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-04-28 03:17:30 +0700

    add: topic parameter to onMutateSwitchHandler

--- src/hooks/useSmartLamp.ts
+++ src/hooks/useSmartLamp.ts
@@ -1,4 +1,5 @@
 import { mqqtApi } from "@/api/mqqt";
+import { LampTopic } from "@/api/mqqt/types";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const SMART_LAMP_KEY = {
@@ -15,7 +16,7 @@ export function useSmartLampQuery() {
 export function useSwitchSmartLampQuery() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: () => mqqtApi.switchState(),
+    mutationFn: (topic: LampTopic) => mqqtApi.switchCurrentState(topic),
     onSuccess: () => queryClient.invalidateQueries(SMART_LAMP_KEY.getState),
   });
 }
